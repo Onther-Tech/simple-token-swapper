a3118598c814cb0978a09434bbe8c0426c037b5a
4000D <dkdkajej@gmail.com>
2018-05-14 16:22:25 +0900

    fix bugs

--- contracts/SimpleSwapper.sol
+++ contracts/SimpleSwapper.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.4.23;
 
 
-import "./openzeppelin-solidity/contracts/ownership/CanReclaimToken.sol";
-import "./openzeppelin-solidity/contracts/ownership/HasNoEther.sol";
-import "./openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/ownership/CanReclaimToken.sol";
+import "openzeppelin-solidity/contracts/ownership/HasNoEther.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 /// @notice SimpleSwapper swap 2 different tokens with same amount.
 ///  SimpleSwapper should have enough token amount of sourceToken
@@ -18,8 +18,8 @@ contract SimpleSwapper is CanReclaimToken, HasNoEther {
 
     require(balance >= approve);
 
-    require(targetToken.transferFrom(msg.sender, this, value));
-    require(sourceToken.transfer(msg.sender, value));
+    require(targetToken.transferFrom(msg.sender, this, approve));
+    require(sourceToken.transfer(msg.sender, approve));
 
     return true;
   }
