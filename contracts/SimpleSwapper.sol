cc35139a6049ea9ea2217d13069dcea0f0810e68
4000D <dkdkajej@gmail.com>
2018-05-14 16:17:22 +0900

    init

---
+++ contracts/SimpleSwapper.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.23;
+
+
+import "./openzeppelin-solidity/contracts/ownership/CanReclaimToken.sol";
+import "./openzeppelin-solidity/contracts/ownership/HasNoEther.sol";
+import "./openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "./openzeppelin-solidity/contracts/math/Math.sol";
+
+
+/// @notice SimpleSwapper swap 2 different tokens with same amount.
+///  SimpleSwapper should have enough token amount of sourceToken
+///  to serve swap service.
+contract SimpleSwapper is CanReclaimToken, HasNoEther {
+  using Math for *;
+
+  /// @notice swap sourceToken from Swapper and targetToken from msg.sender.
+  function swap(ERC20 sourceToken, ERC20 targetToken) public returns(bool) {
+    require(address(sourceToken) != address(targetToken));
+
+    uint256 approve = targetToken.allowance(msg.sender, this);
+    uint256 balance = sourceToken.balanceOf(address(this));
+
+    require(balance >= approve);
+
+    require(targetToken.transferFrom(msg.sender, this, value));
+    require(sourceToken.transfer(msg.sender, value));
+
+    return true;
+  }
+}
