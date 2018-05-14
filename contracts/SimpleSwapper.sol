6968f8f1e674fce3d08b3b9ab94a452865e88f71
4000D <dkdkajej@gmail.com>
2018-05-14 16:19:36 +0900

    remove unused library

--- contracts/SimpleSwapper.sol
+++ contracts/SimpleSwapper.sol
@@ -4,15 +4,11 @@ pragma solidity ^0.4.23;
 import "./openzeppelin-solidity/contracts/ownership/CanReclaimToken.sol";
 import "./openzeppelin-solidity/contracts/ownership/HasNoEther.sol";
 import "./openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
-import "./openzeppelin-solidity/contracts/math/Math.sol";
-
 
 /// @notice SimpleSwapper swap 2 different tokens with same amount.
 ///  SimpleSwapper should have enough token amount of sourceToken
 ///  to serve swap service.
 contract SimpleSwapper is CanReclaimToken, HasNoEther {
-  using Math for *;
-
   /// @notice swap sourceToken from Swapper and targetToken from msg.sender.
   function swap(ERC20 sourceToken, ERC20 targetToken) public returns(bool) {
     require(address(sourceToken) != address(targetToken));
